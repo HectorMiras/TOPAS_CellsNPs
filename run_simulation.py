5f97a71e80af02f6b5f8ce115d0ccd9a3be4a3e1
HectorMiras <hector.miras@gmail.com>
2023-05-18 23:59:38 -0400

    Move run_simulation.py to pythonscripts folder

--- run_simulation.py
+++
@@ -1,8 +0,0 @@
-from pythonscripts.simulation_manager import Simulation_manager
-import json
-import os
-
-config_file = "simulation_config.json"
-smanager = Simulation_manager(os.getcwd(), config_file)
-
-smanager.map_files()
