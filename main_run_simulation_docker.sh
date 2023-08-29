a4390bd9b715bde9fa8b5497626344003a18fb82
HMiras <hector.miras@gmail.com>
2023-08-29 23:10:14 +0200

    Specific main bash script for docker

---
+++ main_run_simulation_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CURRENTPATH=`pwd`
+RUNDIR="${CURRENTPATH}/work/${1:-TestRunDir}"
+CONFIGFILE="SimulationConfigFile.txt"
+mkdir -p $RUNDIR
+
+#source ./venv/bin/activate
+python3 pythonscripts/run_simulation.py $CURRENTPATH
+
+cp $CONFIGFILE $RUNDIR
+cp -r ./runFiles/* $RUNDIR
+#cp -r sourcePHSPFiles $RUNDIR
+
+#cp -r ./supportFiles/* "${RUNDIR}/supportFiles"
+cd $RUNDIR
+
+#dos2unix submit_script.sh
+chmod +x submit_script.sh
+./submit_script.sh
+
+#deactivate
