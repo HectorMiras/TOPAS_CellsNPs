664e4c5e4374c130d3c1f552150cdd2f22a866a6
HMiras <hector.miras@gmail.com>
2023-08-30 12:45:47 +0200

    new scripts and modifications to run on docker and batch

--- bashscripts/run_docker_script.sh
+++ bashscripts/run_docker_script.sh
@@ -13,13 +13,8 @@ ADDITION=0
 
 USER=`whoami`
 CURRENTPATH=`pwd`
-COUNT=0
+COUNT=$1
 
-if [[ -z $ADDITION ]]; then
-    COUNT=0
-else
-    COUNT=$ADDITION
-fi
 
 if [ ! $DOSAMPLE ]; then
 	cd ./supportFiles
@@ -28,58 +23,36 @@ if [ ! $DOSAMPLE ]; then
 	cd $CURRENTPATH
 fi
 
-while [[ $COUNT -lt $((ITER + ADDITION)) ]]
-do
-    #USER=`whoami`
-    #CURRENTPATH=`pwd`
-    DATEDAY=`date | awk '{print $3}'`
-    DATEMONTH=`date | awk '{print $2}'`
-    DATEYEAR=`date | awk '{print $6}'`
-    DATEHOUR=`date | awk '{print $4}' | awk -F: '{print $1}'`
-    DATEMIN=`date | awk '{print $4}' | awk -F: '{print $2}'`
-    DATE=$DATEYEAR$DATEMONTH$DATEDAY
-    UNAME=`uname`
+DIR="${CURRENTPATH}/run${COUNT}"
+echo $COUNT
+echo $DIR
+mkdir -p $DIR
     
-    #DIR=$CURRENTPATH/$DATE/$INFILE/$COUNT
-    #if [ -d $DIR ]
-    #then
-    #   echo Directory exists, removing and recreating $DIR
-    #    rm -rf $DIR
-    #fi
+cp $INFILE1 $DIR
+cp $INFILE2 $DIR
+cp $INFILE3 $DIR    
 
-    DIR="${CURRENTPATH}/run${COUNT}"
-    echo $COUNT
-	echo $DIR
-    mkdir -p $DIR
-    
-    cp $INFILE1 $DIR
-    cp $INFILE2 $DIR
-    cp $INFILE3 $DIR    
-
-    SEED=`bash -c 'echo $RANDOM'`
-    sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE1
-    sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE2
-	sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE3
+SEED=`bash -c 'echo $RANDOM'`
+sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE1
+sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE2
+sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE3
     
 
-    cp -r ./supportFiles $DIR
-#	cp -r sourcePHSPFiles $DIR
+cp -r ./supportFiles $DIR
 	
-	if $DOSAMPLE; then
-		cd "${DIR}/supportFiles"
-		python3 $PYFILE1
-		python3 $PYFILE2
-	fi
-	#read -p "Press any key to continue..."
-	
-	cd $DIR
-	time /topas/bin/topas $INFILE1
-	time /topas/bin/topas $INFILE2
-	time /topas/bin/topas $INFILE3
-	rm $DELFILE
+if $DOSAMPLE; then
+    cd "${DIR}/supportFiles"
+    python3 $PYFILE1
+    python3 $PYFILE2
+fi
 
-    
-    COUNT=$((COUNT+1))
+#read -p "Press any key to continue..."
 	
-	cd $CURRENTPATH
-done
+cd $DIR
+time /topas/bin/topas $INFILE1
+time /topas/bin/topas $INFILE2
+time /topas/bin/topas $INFILE3
+rm $DELFILE
+
+
+cd $CURRENTPATH
--- main_run_simulation_docker.sh
+++ main_run_simulation_docker.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 CURRENTPATH=`pwd`
-RUNDIR="${CURRENTPATH}/work/${1:-TestRunDir}"
+NODE_ID=$1
+RUNDIR="${CURRENTPATH}/work/${2:-RunDir}"
 CONFIGFILE="SimulationConfigFile.txt"
 mkdir -p $RUNDIR
 
-#source ./venv/bin/activate
 python3 pythonscripts/run_simulation.py $CURRENTPATH
 
 cp $CONFIGFILE $RUNDIR
 cp -r ./runFiles/* $RUNDIR
-#cp -r sourcePHSPFiles $RUNDIR
 
-#cp -r ./supportFiles/* "${RUNDIR}/supportFiles"
 cd $RUNDIR
 
-#dos2unix submit_script.sh
-chmod +x submit_script.sh
+chmod +x submit_script.sh $NODE_ID
 ./submit_script.sh
 
-#deactivate
