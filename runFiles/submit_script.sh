243628c02f350439819f016e579f9a1fca739f33
HMiras <hector.miras@gmail.com>
2023-09-01 18:02:46 +0200

    master commit in ubuntu

--- runFiles/submit_script.sh
+++ runFiles/submit_script.sh
@@ -12,8 +12,13 @@ ADDITION=0
 
 USER=`whoami`
 CURRENTPATH=`pwd`
-COUNT=$1
+COUNT=0
 
+if [[ -z $ADDITION ]]; then
+    COUNT=0
+else
+    COUNT=$ADDITION
+fi
 
 if [ ! $DOSAMPLE ]; then
 	cd ./supportFiles
@@ -22,36 +27,58 @@ if [ ! $DOSAMPLE ]; then
 	cd $CURRENTPATH
 fi
 
-DIR="${CURRENTPATH}/run${COUNT}"
-echo $COUNT
-echo $DIR
-mkdir -p $DIR
+while [[ $COUNT -lt $((ITER + ADDITION)) ]]
+do
+    #USER=`whoami`
+    #CURRENTPATH=`pwd`
+    DATEDAY=`date | awk '{print $3}'`
+    DATEMONTH=`date | awk '{print $2}'`
+    DATEYEAR=`date | awk '{print $6}'`
+    DATEHOUR=`date | awk '{print $4}' | awk -F: '{print $1}'`
+    DATEMIN=`date | awk '{print $4}' | awk -F: '{print $2}'`
+    DATE=$DATEYEAR$DATEMONTH$DATEDAY
+    UNAME=`uname`
     
-cp $INFILE1 $DIR
-cp $INFILE2 $DIR
-#cp $INFILE3 $DIR    
+    #DIR=$CURRENTPATH/$DATE/$INFILE/$COUNT
+    #if [ -d $DIR ]
+    #then
+    #   echo Directory exists, removing and recreating $DIR
+    #    rm -rf $DIR
+    #fi
 
-SEED=`bash -c 'echo $RANDOM'`
-sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE1
-sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE2
-#sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE3
+    DIR="${CURRENTPATH}/run${COUNT}"
+    echo $COUNT
+	echo $DIR
+    mkdir -p $DIR
+    
+    cp $INFILE1 $DIR
+    cp $INFILE2 $DIR
+#    cp $INFILE3 $DIR    
+
+    SEED=`bash -c 'echo $RANDOM'`
+    sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE1
+    sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE2
+#	sed -i "s/i:Ts\/Seed = .*/i:Ts\/Seed = $SEED/" $DIR/$INFILE3
     
 
-cp -r ./supportFiles $DIR
+    cp -r ./supportFiles $DIR
+	cp -r sourcePHSPFiles $DIR
 	
-if $DOSAMPLE; then
-    cd "${DIR}/supportFiles"
-    python3 $PYFILE1
-    python3 $PYFILE2
-fi
-
-#read -p "Press any key to continue..."
+	if $DOSAMPLE; then
+		cd "${DIR}/supportFiles"
+		python3 $PYFILE1
+		python3 $PYFILE2
+	fi
+	#read -p "Press any key to continue..."
 	
-cd $DIR
-time /topas/bin/topas $INFILE1
-time /topas/bin/topas $INFILE2
-#time /topas/bin/topas $INFILE3
-rm $DELFILE
+	cd $DIR
+	time ~/topas/bin/topas $INFILE1
+	time ~/topas/bin/topas $INFILE2
+#	time ~/topas/bin/topas $INFILE3
+	rm $DELFILE
 
-
-cd $CURRENTPATH
+    
+    COUNT=$((COUNT+1))
+	
+	cd $CURRENTPATH
+done
